1b4f8b0ffffb30dee32c47df421ca826d1bed0e3
Kevin <kevin.r.neely@outlook.com>
2018-04-19 11:52:28 -0400

    Add project files.

---
+++ BibleMVC/Controllers/ApiController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+
+namespace BibleMVC.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Api")]
+    public class ApiController : Controller
+    {
+        public const string BaseUrl = "http://labs.bible.org/api/?passage=John+3:16-17&formatting=full&type=json";
+
+        public T Execute<T>(RestRequest request) where T : new()
+        {
+            var client = new RestClient();
+            client.BaseUrl = new System.Uri(BaseUrl);
+            var response = client.Execute<T>(request);
+
+            if (response.ErrorException != null)
+            {
+                const string message = "Error retrieving response.  Check inner details for more info.";
+                var bibleException = new ApplicationException(message, response.ErrorException);
+                throw bibleException;
+            }
+            return response.Data;
+        }
+    }
+}
